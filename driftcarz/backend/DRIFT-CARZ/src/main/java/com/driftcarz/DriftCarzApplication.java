b0d0f64f6040b98a70f0bf8fd7b94adb2ed14bcd
Sonu Singh <grjdeveloper@gmail.com>
2023-07-18 09:10:24 +0530

    setting up the backend
    setting up the backend

---
+++ driftcarz/backend/DRIFT-CARZ/src/main/java/com/driftcarz/DriftCarzApplication.java
@@ -0,0 +1,13 @@
+package com.driftcarz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DriftCarzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DriftCarzApplication.class, args);
+	}
+
+}
---
+++ driftcarz/backend/DRIFT-CARZ/src/test/java/com/driftcarz/DriftCarzApplicationTests.java
@@ -0,0 +1,13 @@
+package com.driftcarz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DriftCarzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
